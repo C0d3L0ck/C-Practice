5f88b970eb3ef51f1c9b2c40bac1b4f717a0541c
CodeLock <105467967+C0d3L0ck@users.noreply.github.com>
2023-06-23 22:38:47 -0400

    Add files via upload

---
+++ ConsoleLearning/Interfaces/IRun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace C_8.Interfaces
+{
+    public interface IRun
+    {
+        public static void Run(string name)
+        {
+            Console.WriteLine(name + " is running");
+        }
+    }
+}
